f42c5e09908023937b8ed705f28a2fa578d87537
Christopher Stawarz <cstawarz@mit.edu>
2014-04-04 15:03:46 -0400

    Run nightly build only on weekdays.  Removed coxlab_config.py.  Minor tweaks.

--- dicarlolab_config.py
+++ dicarlolab_config.py
@@ -1,8 +1,8 @@
 # To use this configuration, create a symbolic link to this file named
 # "local_config.py"
 
-buildbot_hostname = 'dicarlo-mwdev.mit.edu'
-buildbot_admin = 'Christopher Stawarz <cstawarz@mit.edu>'
+master_hostname = 'dicarlo-mwdev.mit.edu'
+master_admin = 'Christopher Stawarz <cstawarz@mit.edu>'
 webstatus_port = 8010
 
 from local_passwords import slave_pass, webstatus_pass
